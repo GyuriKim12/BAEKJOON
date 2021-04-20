4781176fb50a4798ea719cb06f448ae99a7cd2c5
GyuriKim12 <kralicia@naver.com>
2021-04-20 17:15:40 +0900

    commit

--- 함수.py
+++ 함수.py
@@ -1,16 +1,16 @@
 #정수n개가 주어졌을 때, n개의 합을 구하는 함수를 작성하시오
-def print_sum(result):
+def print_sum():
     count=0
     numbers=list()
+    i=0
     while count<x:
         s=input('정수를 입력하시오: ')
         numbers.append(int(s))
         count+=1
-    i=0
-    while count<x:
-        result+=numbers[i]
-        count+=1 
-        i+=1  
-    return result
+    sum=0
+    while i<len(numbers):
+        sum+=numbers[i]
+        i+=1
+    return sum
 x=int(input('정수의 개수를 입력하시오: '))
-print_sum(result)
+print_sum()
